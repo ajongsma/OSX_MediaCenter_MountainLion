5f5967dcb0744627a26c7ac45ae05d949cfc3fb9
ajongsma <a.jongsma@gmail.com>
2013-02-07 12:51:56 +0100

    Update scripts/install_brew.sh

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -8,15 +8,6 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
-#brew install bash-completion git
-## Install GNU core utilities (those that come with OS X are outdated)
-#brew install coreutils
-
-## Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-#brew install findutils
-## Install Bash 4
-#brew install bash
-
 # Remove outdated versions from the cellar
 brew cleanup
 
