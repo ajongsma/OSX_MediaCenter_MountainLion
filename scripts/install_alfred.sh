98ec03c0627996d074e05b5efec70d5284ac7ded
ajongsma <a.jongsma@gmail.com>
2013-02-07 13:02:26 +0100

    Update scripts/install_alfred.sh

--- scripts/install_alfred.sh
+++ scripts/install_alfred.sh
@@ -1,20 +1,37 @@
-#!/bin/bash
-# Download and install Alfred
+#!/usr/bin/env bash
 
-if [ ! -d /Applications/alfred.app ]; then
-  #url="$(curl -s http://www.alfredapp.com | grep '.dmg')"
+BOLD=$(tput bold)
+BLACK=$(tput setaf 0) #   0  Black
+RED=$(tput setaf 1)  #  1   Red
+GREEN=$(tput setaf 2)  #    2   Green
+YELLOW=$(tput setaf 3)  #   3   Yellow
+BLUE=$(tput setaf 4)  #     4   Blue
+MAGENTA=$(tput setaf 5)  #  5   Magenta
+CYAN=$(tput setaf 6)  #     6   Cyan
+WHITE=$(tput setaf 7)  #    7   White
+RESET=$(tput sgr0)
+col=40
+
+## Download and install Alfred
+if [ ! -e /Applications/alfred.app ] ; then
+  #echo "Alfred not installed, please install..."
+  printf 'Alfred not installed, please install..' "$RED" $col '[FAIL]' "$RESET"
+
   url="http://cachefly.alfredapp.com/alfred_1.3.2_265.zip"
   zip="${url##http*/}"
   download_dir="/src/alfred"
   mkdir -p "$download_dir"
-
-  #sudo download_dmg "$(curl -s http://www.alfredapp.com | grep '.dmg')" "${download_dir}/${zip}"
-
-  echo "Downloaded to: ${download_dir}/${zip}"
-
   curl -L "$url" -o "${download_dir}/${zip}"
+
+  printf 'Downloaded to ${download_dir}/${zip}, installing...' "YELLOW" $col '[WAIT]' "$RESET"
   unzip -q "${download_dir}/${zip}" -d /${download_dir}/
   sudo mv ${download_dir}/alfred.app /Applications/alfred.app
+
+  if [ ! -e /Applications/alfred.app ] ; then
+    else
+    printf 'Alfred installed' "$GREEN" $col '[OK]' "$RESET"
+  fi
 else
-  echo "$(tput setaf 10)Alfred is already installed.$(tput sgr0)"
+  #echo "Alfred found                               [OK]"
+  printf 'Alfred found' "$GREEN" $col '[OK]' "$RESET"
 fi
