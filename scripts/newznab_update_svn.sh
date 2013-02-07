6b354994039061e0d53aeb9e4b23f2f3327b65b0
ajongsma <a.jongsma@gmail.com>
2013-02-07 13:18:34 +0100

    Update scripts/newznab_update_svn.sh

--- scripts/newznab_update_svn.sh
+++ scripts/newznab_update_svn.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/env bash
 
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
+source ../defaults.sh
+clear
+
+if [[ $AGREED == "no" ]]; then
+        echo "Please edit the defaults.sh file"
+        exit
+fi
+
 ### EDIT_THESE ###
-export NEWZNAB_PATH="/User/Newznab/Sites/newznab"
+#export INST_NEWZNAB_PATH="/User/Newznab/Sites/newznab"
 export NEWZNAB_SVN_USERID="svnplus"
 export NEWZNAB_SVN_PASSWORD="password"
 
-svn co --force --username $NEWZNAB_SVN_USERID --password $NEWZNAB_SVN_PASSWORD svn://svn.newznab.com/nn/branches/nnplus $NEWZNAB_PATH/
+echo svn co --force --username $NEWZNAB_SVN_USERID --password $NEWZNAB_SVN_PASSWORD svn://svn.newznab.com/nn/branches/nnplus $INST_NEWZNAB_PATH/
+svn co --force --username $NEWZNAB_SVN_USERID --password $NEWZNAB_SVN_PASSWORD svn://svn.newznab.com/nn/branches/nnplus $INST_NEWZNAB_PATH/
 
-cd $NEWZNAB_PATH"/misc/update_scripts"
+cd $INST_NEWZNAB_PATH"/misc/update_scripts"
 #php5 update_database_version.php
 php update_database_version.php
 
-cd $NEWZNAB_PATH"/misc/update_scripts/nix_scripts/tmux/scripts"
-./newzmab_set_perms.sh
+#cd $INST_NEWZNAB_PATH"/misc/update_scripts/nix_scripts/tmux/scripts"
+./newznab_set_perms.sh
