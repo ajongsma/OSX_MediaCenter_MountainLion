7bb1788db73983760d787ca0354bab8ddb668961
ajongsma <a.jongsma@gmail.com>
2013-02-11 04:27:50 -0800

    Create check_updates.sh

---
+++ bin/check_updates.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "-----------------------------------------------------------"
+echo "| Checking for updates on GIT
+echo "-----------------------------------------------------------"
+cd ~/Github/
+
+
+
+echo "-----------------------------------------------------------"
+echo "| Checking for updates on SVN
+echo "-----------------------------------------------------------"
+cd /Users/Newznab/Sites/newsnab
+#svn update
