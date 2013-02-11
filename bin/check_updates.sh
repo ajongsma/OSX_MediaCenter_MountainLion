4fb84b926a85360a58b33dcf53a4068df33007a0
ajongsma <a.jongsma@gmail.com>
2013-02-11 14:04:49 +0100

    Update bin/check_updates.sh

--- bin/check_updates.sh
+++ bin/check_updates.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 echo "-----------------------------------------------------------"
-echo "| Checking for updates on GIT
+echo "| Checking for updates on GIT"
 echo "-----------------------------------------------------------"
 cd ~/Github/
 
 
 
 echo "-----------------------------------------------------------"
-echo "| Checking for updates on SVN
+echo "| Checking for updates on SVN"
 echo "-----------------------------------------------------------"
+echo "| Checking for updates NewzNAB:"
 cd /Users/Newznab/Sites/newsnab
+svn info
 #svn update
