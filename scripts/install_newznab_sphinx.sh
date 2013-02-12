2b3f819461747b115ef816a2d71cfa07a9d71217
ajongsma <a.jongsma@gmail.com>
2013-02-12 15:34:14 +0100

    Update scripts/install_newznab_sphinx.sh

--- scripts/install_newznab_sphinx.sh
+++ scripts/install_newznab_sphinx.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
 #------------------------------------------------------------------------------
-# Install Sphinx for NewzNAB 
+# Install Sphinx for NewzNAB
 #------------------------------------------------------------------------------
+## http://newznab.readthedocs.org/en/latest/misc/sphinx/
+## /Users/Newznab/Sites/newznab/db/sphinxdata/sphinx.conf
 
 source ../config.sh
 
-## http://newznab.readthedocs.org/en/latest/misc/sphinx/
+if [ -e /usr/local/bin/searchd ] ; then
+    printf 'local found\n' "$GREEN" $col '[OK]' "$RESET"
+else
+    printf 'Sphinx not installed, installing…\n' "$RED" $col '[FAIL]' "$RESET"
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+    exit
+fi
 
-## ?? brew install sphinx
-## /Users/Newznab/Sites/newznab/db/sphinxdata/sphinx.conf
-
-echo "Download latest Sphinx from http://sphinxsearch.com"
-#open http://sphinxsearch.com/
-
-## which searchd
-## /usr/local/bin/searchd
-## which indexer
-## /usr/local/bin/indexer
-
-cd /Users/Newznab/Sites/newznab/misc/sphinx
+cd $INST_NEWZNAB_PATH/misc/sphinx
 ./nnindexer.php generate
 
 echo "Creating Lauch Agent file:"
@@ -52,20 +50,36 @@ launchctl load ~/Library/LaunchAgents/com.nnindexer.nnindexer.plist
 ./nnindexer.php daemon --stop
 ./nnindexer.php daemon
 
-#### ERR:
+
+#### ERR: ###
 ## WARNING: index 'releases': preload: failed to open /Users/Newznab/Sites/newznab/db/sphinxdata/releases.sph: No such file or directory; NOT SERVING
 ## precaching index 'releases_delta'
-
-## /Users/Newznab/Sites/newznab/db/sphinxdata
+##   /Users/Newznab/Sites/newznab/db/sphinxdata
 ## indexer --config /Users/Newznab/Sites/newznab/db/sphinxdata/sphinx.conf --all
-
 ## ./nnindexer.php search --index releases "some search term"
+##   /Users/Newznab/Sites/newznab/db/sphinxdata/releases.sph
+#############
 
+if [ -e /Users/Newznab/Sites/newznab/db/sphinxdata/releases.sph ] ; then
+    printf 'Sphinx data releases.sph found\n' "$GREEN" $col '[OK]' "$RESET"
+else
+    printf 'Sphinx data releases.sph not found, initializing full indexing\n' "$YELLOW" $col '[WAIT]' "$RESET"
+    indexer --config /Users/Newznab/Sites/newznab/db/sphinxdata/sphinx.conf --all
+    ./nnindexer.php index full all
+    ./nnindexer.php index delta all
+    ./nnindexer.php daemon --stop
+    ./nnindexer.php daemon
+fi
 
 echo "-----------------------------------------------------------"
 echo "| Configure Sphinx:"
 echo "| Use Sphinx                 : Yes"
-echo "| Sphinx Configuration Path  : <full path to sphinx.conf>"
+#echo "| Sphinx Configuration Path  : /Users/Newznab/Sites/newznab/db/sphinxdata/sphinx.conf"
+echo "| Sphinx Configuration Path  : $INST_NEWZNAB_PATH/db/sphinxdata/sphinx.conf"
 echo "| Sphinx Binaries Path       : /usr/local/bin/"
 echo "-----------------------------------------------------------"
 open http://localhost/newznab/admin
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install Sphinx for NewzNAB complete."
+echo "#------------------------------------------------------------------------------"
