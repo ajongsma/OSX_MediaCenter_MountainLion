c6afae9b7e47aa16fa6155eb9d6f063bf3516c30
ajongsma <a.jongsma@gmail.com>
2013-02-12 05:33:54 -0800

    Create install_newznab_tmux.sh

---
+++ scripts/install_newznab_tmux.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+source ../config.sh
+
+echo "-----------------------------------------------------------"
+echo "| Install NewzNAB jonnyboy/newznab-tmux"
+echo "-----------------------------------------------------------"
+## https://github.com/jonnyboy/newznab-tmux.git
+
+brew install htop
+brew install iftop
+brew install watch
+
+cd /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/
+git clone https://github.com/jonnyboy/newznab-tmux.git tmux
+cd /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/tmux
+
+echo "-----------------------------------------------------------"
+echo "| Backing up current MySQL database..."
+echo "-----------------------------------------------------------"
+mysqldump --opt -u root -p newznab > ~/newznab_backup.sql
+
+echo "-----------------------------------------------------------"
+echo "| Change the following settings:"
+echo "| export NEWZPATH="/var/www/newznab" : export NEWZPATH="/Users/Newznab/Sites/newznab""
+echo "| export BINARIES="false"            : export BINARIES="true""
+echo "| *export BINARIES_THREADS="false"   : export BINARIES_THREADS="true""
+echo "| export RELEASES="false"            : export RELEASES="true""
+echo "| export OPTIMIZE="false"            : export OPTIMIZE="true""
+echo "| export CLEANUP="false"             : export CLEANUP="true""
+echo "| export PARSING="false"             : export PARSING="true""
+echo "| export SPHINX="true"               : export SPHINX="true""
+echo "| export SED="/bin/sed"              : export SED="/usr/local/bin/gsed""
+echo "| "
+echo "| TESTING:"
+echo "| export USE_HTOP="false"            : export USE_HTOP="true""
+echo "| export USE_NMON="false"            : ??" 
+echo "| export USE_BWMNG="false"           : ??"
+echo "| export USE_IOTOP="false"           : ??"
+echo "| export USE_MYTOP="false"           : ??"
+echo "| export USE_VNSTAT="false"          : ??"
+echo "| export USE_IFTOP="false"           : export USE_IFTOP="true""
+echo "| export POWERLINE="false"           : ??"
+echo "| "
+echo "| Use tmpfs to run postprocessing on true/false"
+echo "| export RAMDISK="false"             : ??"
+
+echo "| export AGREED="no"                 : export AGREED="yes""
+echo "-----------------------------------------------------------"
+cp config.sh defaults.sh
+subl defaults.sh
+
+echo "-----------------------------------------------------------"
+echo "| Change the following settings:"
+echo "| export NEWZPATH="/var/www/newznab" : export NEWZPATH="/Users/Newznab/Sites/newznab""
+echo "-----------------------------------------------------------"
+subl /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/tmux/scripts/check_svn.sh 
+
+echo "-----------------------------------------------------------"
+export NEWZPATH="/Users/Newznab/Sites/newznab"
+export PASSWORD="<password>"
+echo "-----------------------------------------------------------"
+subl /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/tmux/scripts/update_svn.sh 
+
+cd /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/tmux/scripts
+sudo ./set_perms.sh
+
+cd /Users/Newznab/Sites/newznab/misc/update_scripts/nix_scripts/tmux/
+./start.sh
+
+### ERR:
+## PHP Fatal error:  Call to a member function fetch_assoc() on a non-object in /Users/Newznab/Sites/newznab/www/lib/framework/db.php on line 193
+## PHP Stack trace:
+## PHP   1. {main}() /Users/Newznab/Sites/newznab/misc/update_scripts/update_releases.php:0
+## PHP   2. Sphinx->update() /Users/Newznab/Sites/newznab/misc/update_scripts/update_releases.php:10
+## PHP   3. DB->getAssocArray() /Users/Newznab/Sites/newznab/www/lib/sphinx.php:322
+##
+## Fatal error: Call to a member function fetch_assoc() on a non-object in /Users/Newznab/Sites/newznab/www/lib/framework/db.php on line 193
+########
