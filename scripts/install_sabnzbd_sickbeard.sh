07a61463470db461b646f43692e83a7c6cbde7c7
ajongsma <a.jongsma@gmail.com>
2013-02-13 05:44:45 -0800

    Create install_sabnzbd_sickbeard.sh

---
+++ scripts/install_sabnzbd_sickbeard.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing SABnzbd for Sick-Beard"
+echo "#------------------------------------------------------------------------------"
+
+source ../config.sh
+
+echo "-----------------------------------------------------------"
+echo "| Menu, Config, Categories:"
+echo "| tv, Default, Default, nzbToSickbeard.py"
+echo "-----------------------------------------------------------"
+open http://localhost:8080/config/switches/
+echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+read -n 1 -s
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing SABnzbd for Sick-Beard - Complete"
+echo "#------------------------------------------------------------------------------"
