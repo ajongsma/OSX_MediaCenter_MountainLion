a927d3e568423ff7dde61568ab419ad0715ea80b
ajongsma <a.jongsma@gmail.com>
2013-02-13 09:48:13 +0100

    Update scripts/install_cheetah.sh

--- scripts/install_cheetah.sh
+++ scripts/install_cheetah.sh
@@ -6,8 +6,16 @@ echo "#-------------------------------------------------------------------------
 
 source ../config.sh
 
-echo "Download latest Cheetah:"
+echo "Download latest Cheetah from http://www.cheetahtemplate.org/download"
 open http://www.cheetahtemplate.org/download
+
+cd ~/Downloads
+#curl -O http://sphinxsearch.com/files/sphinx-2.0.6-release.tar
+while ( [ ! -e Cheetah-2.4.4.tar ] )
+do
+    printf 'Waiting for Cheetah to be downloaded…\n' "YELLOW" $col '[WAIT]' "$RESET"
+    sleep 15
+done
 
 tar xvzf Cheetah-2.4.4.tar
 cd Cheetah-2.4.4.tar
