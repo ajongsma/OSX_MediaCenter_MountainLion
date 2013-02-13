50fd4dc9bc210caefe4d220970bf9046a20085e0
ajongsma <a.jongsma@gmail.com>
2013-02-13 10:38:22 +0100

    Update scripts/install_sabnzbd.sh

--- scripts/install_sabnzbd.sh
+++ scripts/install_sabnzbd.sh
@@ -25,19 +25,19 @@ fi
 
 echo "-----------------------------------------------------------"
 echo "| News Server Setup:"
-echo "| Server            : $INST_NEWSSERVER_SERVER"
-echo "| Port              : $INST_NEWSSERVER_SERVER_PORT_SSL"
-echo "| User Name         : $INST_NEWSSERVER_SERVER_UID"
-echo "| Password          : $INST_NEWSSERVER_SERVER_PW"
-echo "| SSL               : Enable"
+echo "| Server                                  : $INST_NEWSSERVER_SERVER"
+echo "| Port                                    : $INST_NEWSSERVER_SERVER_PORT_SSL"
+echo "| User Name                               : $INST_NEWSSERVER_SERVER_UID"
+echo "| Password                                : $INST_NEWSSERVER_SERVER_PW"
+echo "| SSL                                     : Enable"
 echo "-----------------------------------------------------------"
 echo "| Step 2"
-echo "| Access            : I want SABnzbd to be viewable by any pc on my network."
-echo "| Password protect access to SABnzbd : Enable"
-echo "| User Name         : $INST_SABNZBD_UID"
-echo "| Password          : $INST_SABNZBD_PW"
-echo "| HTTPS             : Disable"
-echo "| Launch            : Disable"
+echo "| Access                                  : I want SABnzbd to be viewable by any pc on my network."
+echo "| Password protect access to SABnzbd      : Enable"
+echo "| User Name                               : $INST_SABNZBD_UID"
+echo "| Password                                : $INST_SABNZBD_PW"
+echo "| HTTPS                                   : Disable"
+echo "| Launch                                  : Disable"
 echo "-----------------------------------------------------------"
 open /Applications/SABnzbd.app 
 #echo -e "${BLUE} --- press any key to continue --- ${RESET}"
@@ -46,12 +46,12 @@ open /Applications/SABnzbd.app
 mkdir /Users/Andries/Library/Application\ Support/scripts
 echo "-----------------------------------------------------------"
 echo "| Folders:"
-echo "| Temporary Download Folder : ~/Downloads/Usenet/Incomplete"
-echo "| Minimum Free Space        : 1G"
-echo "| Completed Download Folder : ~/Downloads/Usenet/Complete"
-echo "| Watched Folder            : ~/Downloads/Usenet/Watch"
-echo "| Watched Folder Scan Speed : 300"
-echo "| Post Processing Folder    : ~/Library/Application Support/SABnzbd/scripts"
+echo "| Temporary Download Folder               : ~/Downloads/Usenet/Incomplete"
+echo "| Minimum Free Space                      : 1G"
+echo "| Completed Download Folder               : ~/Downloads/Usenet/Complete"
+echo "| Watched Folder                          : ~/Downloads/Usenet/Watch"
+echo "| Watched Folder Scan Speed               : 300"
+echo "| Post Processing Folder                  : ~/Library/Application Support/SABnzbd/scripts"
 echo "-----------------------------------------------------------"
 http://localhost:8080/sabnzbd/config/
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
@@ -85,29 +85,6 @@ echo "-----------------------------------------------------------"
 open http://localhost:8080/sabnzbd/config/categories/
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
 read -n 1 -s
-
-echo "Creating Lauch Agent file:"
-cat >> /tmp/com.sabnzbd.SABnzbd.plist <<'EOF'
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  <key>Label</key>
-  <string>com.sabnzbd.SABnzbd</string>
-  <key>ProgramArguments</key>
-  <array>
-     <string>/usr/bin/open</string>
-     <string>-a</string>
-      <string>/Applications/SABnzbd.app</string>
-  </array>
-  <key>RunAtLoad</key>
-  <true/>
-</dict>
-</plist>
-EOF
-
-mv /tmp/com.sabnzbd.SABnzbd.plist ~/Library/LaunchAgents/
-launchctl load ~/Library/LaunchAgents/com.sabnzbd.SABnzbd.plist
 
 source ../config.sh
 #if [[ $INST_SABNZBD_KEY_API == "" ]] || [[ $INST_SABNZBD_KEY_NZB == "" ]]; then
@@ -158,7 +135,6 @@ fi
 #fi
 
 
-## SabNZBD - AFTER SabNZBD INSTALL
 echo "-----------------------------------------------------------"
 echo "| Add SabNZBD support to NewzNAB:
 echo "|*Integration Type              : Site Wide"
@@ -169,6 +145,29 @@ echo "-----------------------------------------------------------"
 open http://localhost/newznab/admin/site-edit.php
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
 read -n 1 -s
+
+echo "Creating Lauch Agent file:"
+cat >> /tmp/com.sabnzbd.SABnzbd.plist <<'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.sabnzbd.SABnzbd</string>
+  <key>ProgramArguments</key>
+  <array>
+     <string>/usr/bin/open</string>
+     <string>-a</string>
+      <string>/Applications/SABnzbd.app</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+</dict>
+</plist>
+EOF
+
+mv /tmp/com.sabnzbd.SABnzbd.plist ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/com.sabnzbd.SABnzbd.plist
 
 
 echo "#------------------------------------------------------------------------------"
