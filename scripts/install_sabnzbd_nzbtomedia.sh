0fe1d93e2701374a3bb9b1284c4424fda246c0ec
ajongsma <a.jongsma@gmail.com>
2013-02-13 10:54:26 +0100

    Update scripts/install_sabnzbd_nzbtomedia.sh

--- scripts/install_sabnzbd_nzbtomedia.sh
+++ scripts/install_sabnzbd_nzbtomedia.sh
@@ -1,2 +1,38 @@
+source ../config.sh
 
-##
+echo "#------------------------------------------------------------------------------"
+echo "# Installing SABnzbd - nzbToMedia"
+echo "#------------------------------------------------------------------------------"
+## https://github.com/clinton-hall/nzbToMedia
+
+[ -d ~/Github ] || mkdir -p ~/Github
+#if [ ! -d ~/Github/ ] ; then
+#    mkdir ~/Github/
+#fi
+
+cd ~/Github/
+
+git clone https://github.com/clinton-hall/nzbToMedia
+cp -R ~/Github/nzbToMedia/* ~/Library/Application\ Support/SABnzbd/scripts/
+#cp /Applications/Sick-Beard/autoProcessTV/* ~/Library/Application\ Support/SABnzbd/scripts/
+
+cd ~/Library/Application\ Support/SABnzbd/scripts/
+if [ ! -f autoProcessMedia.cfg ] ; then
+    cp autoProcessMedia.cfg.sample autoProcessMedia.cfg
+fi
+
+#cp autoProcessTV.cfg.sample autoProcessTV.cfg
+#cp autoProcessMovie.cfg.sample autoProcessMovie.cfg
+#echo "-----------------------------------------------------------"
+#echo "| Modify the following:"
+#echo "| port=8081"
+#echo "| username=couchpotato"
+#echo "| password=<password>"
+#echo "| web_root="
+#echo "-----------------------------------------------------------"
+#subl autoProcessTV.cfg
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing SABnzbd - nzbToMedia - Complete"
+echo "#------------------------------------------------------------------------------"
+
