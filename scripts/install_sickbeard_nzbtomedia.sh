ce7feb5774507ed4013e3ecf2357fbbeab562723
ajongsma <a.jongsma@gmail.com>
2013-02-13 13:25:21 +0100

    Update scripts/install_sickbeard_nzbtomedia.sh

--- scripts/install_sickbeard_nzbtomedia.sh
+++ scripts/install_sickbeard_nzbtomedia.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
 echo "#------------------------------------------------------------------------------"
-echo "# Installing SABnzbd - nzbToMedia"
+echo "# Configuring Sickbeard for SABnzbd - nzbToMedia"
 echo "#------------------------------------------------------------------------------"
 
 source ../config.sh
+
+if [ -f ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg ] ; then
+    printf 'SABnzbd - nzbToMedia installed\n' "$GREEN" $col '[OK]' "$RESET"
+else
+    printf 'SABnzbd - nzbToMedia not installed\n' "$RED" $col '[FAIL]' "$RESET"
+    echo -e "${RED} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+    exit
+fi
+
+echo "-----------------------------------------------------------"
+echo "| Add the following to [SickBeard]:
+echo "| host                                    : localhost"
+echo "| Port                                    : 8081"
+echo "| username                                : $INST_SICKBEARD_UID"
+echo "| password                                : $INST_SICKBEARD_PW"
+echo "-----------------------------------------------------------"
+subl ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
+echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+read -n 1 -s
+
+
+echo "#------------------------------------------------------------------------------"
+echo "# Configuring Sickbeard for SABnzbd - nzbToMedia - Complete"
+echo "#------------------------------------------------------------------------------"
