04a54eb41be2fd27e08d179bce2e0e43f2ad34f0
a.jongsma <a.jongsma@gmail.com>
2013-02-16 17:01:55 +0100

    Marachino modules configs

--- scripts/install_maraschino.sh
+++ scripts/install_maraschino.sh
@@ -17,10 +17,30 @@ osascript -e 'tell app "Terminal"
     do script "python /Applications/maraschino/Maraschino.py"
 end tell'
 
+echo "-----------------------------------------------------------"
+echo "| Click hidden gear icon, top left to configure modules"
+echo "| --------------------"
+echo "| SABnzbd"
+echo "| Hostname            : http://localhost"
+echo "| Port                : $INST_SABNZBD_PORT"
+echo "| API Key             : $INST_SABNZBD_KEY_API"
+echo "----------------------"
+echo "| Sickbeard"
+echo "| API Key             : $INST_SICKBEARD_KEY_API"
+echo "| Username            : $INST_SICKBEARD_UID"
+echo "| Password            : $INST_SICKBEARD_PW"
+echo "| Hostname            : http://localhost"
+echo "| Port                : $INST_SABNZBD_PORT"
+
+echo "----------------------"
+echo "| Couch Potato"
+echo "| API Key             : $INST_COUCHPOTATOD_API"
+echo "| Username            : $INST_COUCHPOTATO_UID"
+echo "| Password            : $INST_COUCHPOTATO_PW"
+echo "| Hostname            : http://localhost"
+echo "| Port                : $INST_COUCHPOTATO_PORT"
+echo "-----------------------------------------------------------"
 open http://localhost:7000
-
-
-#sudo python /Applications/auto-sub/AutoSub.py
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
 read -n 1 -s
 
