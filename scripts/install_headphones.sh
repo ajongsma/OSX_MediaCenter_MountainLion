514a91c80faab35bc5ea9a35970f7400cb44749d
ajongsma <a.jongsma@gmail.com>
2013-02-18 16:56:02 +0100

    Update scripts/install_headphones.sh

--- scripts/install_headphones.sh
+++ scripts/install_headphones.sh
@@ -7,7 +7,7 @@ echo "#-------------------------------------------------------------------------
 source ../config.sh
 
 if [ ! -d $INST_HEADPHONES_PATH ] ; then
-    sudo mkdir -p $INST_SPOTWEB_PATH    
+    sudo mkdir -p $INST_HEADPHONES_PATH    
 fi
 
 #cd $INST_HEADPHONES_PATH
