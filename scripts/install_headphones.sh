acfc707e454df578d24e6e29c84d99b85116de45
ajongsma <a.jongsma@gmail.com>
2013-02-18 16:38:42 +0100

    Update scripts/install_headphones.sh

--- scripts/install_headphones.sh
+++ scripts/install_headphones.sh
@@ -6,4 +6,18 @@ echo "#-------------------------------------------------------------------------
 
 source ../config.sh
 
+if [ ! -d $INST_HEADPHONES_PATH ] ; then
+    sudo mkdir -p $INST_SPOTWEB_PATH    
+fi
 
+#cd $INST_HEADPHONES_PATH
+sudo git git clone https://github.com/rembo10/headphones.git $INST_HEADPHONES_PATH
+sudo chown `whoami`:wheel $INST_HEADPHONES_PATH
+
+open http://localhost:8181
+
+
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing Headphones - Complete"
+echo "#------------------------------------------------------------------------------"
