39cd1ccdef17090de65978f87237e0f73e5f13f6
a.jongsma <a.jongsma@gmail.com>
2013-02-19 23:30:33 +0100

    Minor

--- bin/newznab_cycle.sh
+++ bin/newznab_cycle.sh
@@ -1,38 +1,72 @@
 #!/bin/sh
-# call this script from within screen to get binaries, processes releases and
+# call this script from within screen to get binaries, processes releases and 
 # every half day get tv/theatre info and optimise the database
 
 set -e
 
-export NEWZNAB_PATH="/Users/Newznab/Sites/newznab"
-export NEWZNAB_PATH_UPDATE_SCRIPTS="${NEWZNAB_PATH}/misc/update_scripts"
-export NEWZNAB_PATH_NIX_SCRIPTS="${NEWZNAB_PATH}/misc/update_scripts/nix_scripts"
+export NEWZNAB_PATH="/Users/Newznab/Sites/newznab/misc/update_scripts"
+export NEWZNAB_TESTING_PATH="/Users/Newznab/Sites/newznab/misc/testing"
 export NEWZNAB_SLEEP_TIME="600" # in seconds
 
-while :
- do
-	CURRTIME=`date +%s`
-#	cd ${NEWZNAB_PATH_NIX_SCRIPTS}
-	./newznab_local.sh
+BOLD=$(tput bold)
+BLACK=$(tput setaf 0) #   0  Black
+RED=$(tput setaf 1)  #  1   Red
+GREEN=$(tput setaf 2)  #    2   Green
+YELLOW=$(tput setaf 3)  #   3   Yellow
+BLUE=$(tput setaf 4)  #     4   Blue
+MAGENTA=$(tput setaf 5)  #  5   Magenta
+CYAN=$(tput setaf 6)  #     6   Cyan
+WHITE=$(tput setaf 7)  #    7   White
+RESET=$(tput sgr0)
 
-#	#cd ${NEWZNAB_PATH}
-#	#php ${NEWZNAB_PATH}/retrieve.php
-#
-#	#php ${NEWZNAB_PATH_UPDATE_SCRIPTS}/update_releases.php
-#	#php /var/www/newznab/www/admin/nzb-importmodified.php 
-#	#/var/www/newznab/tempnzbs/ true
-#	cd ${NEWZNAB_PATH_UPDATE_SCRIPTS}
-#	php ${NEWZNAB_PATH_UPDATE_SCRIPTS}/update_releases.php
-#
-#	DIFF=$(($CURRTIME-$LASTOPTIMIZE))
-#	if [ "$DIFF" -gt 43200 ] || [ "$DIFF" -lt 1 ]
-#	then
-#	        LASTOPTIMIZE=`date +%s`
-#	        php ${NEWZNAB_PATH_UPDATE_SCRIPTS}/optimise_db.php
-#	        php ${NEWZNAB_PATH_UPDATE_SCRIPTS}/update_tvschedule.php
-#	        php ${NEWZNAB_PATH_UPDATE_SCRIPTS}/update_theaters.php
-#	fi
-#
-#	echo "$(date) - waiting ${NEWZNAB_SLEEP_TIME} seconds..."
-#	sleep ${NEWZNAB_SLEEP_TIME}
+PRINTF_MASK="%-50s %s %10s %s\n"
+
+LASTOPTIMIZE=`date +%s`
+
+while :
+
+ do
+CURRTIME=`date +%s`
+
+cd ${NEWZNAB_PATH}
+printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_binaries]" "$RESET"
+php ${NEWZNAB_PATH}/update_binaries.php
+printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_binaries]" "$RESET"
+
+echo "---------------------------------------------------------------------------"
+printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_releases]" "$RESET"
+php ${NEWZNAB_PATH}/update_releases.php
+printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
+
+cd ${NEWZNAB_TESTING_PATH}
+echo "---------------------------------------------------------------------------"
+printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_parsing]" "$RESET"
+php ${NEWZNAB_TESTING_PATH}/update_parsing.php
+printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
+
+echo "---------------------------------------------------------------------------"
+printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[removespecial" "$RESET"
+php ${NEWZNAB_TESTING_PATH}/removespecial.php
+printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[removespecial]" "$RESET"
+#php /Users/Newznab/Sites/newznab/misc/testing/update_parsing.php
+
+
+DIFF=$(($CURRTIME-$LASTOPTIMIZE))
+if [ "$DIFF" -gt 43200 ] || [ "$DIFF" -lt 1 ]
+then
+    LASTOPTIMIZE=`date +%s`
+
+	cd ${NEWZNAB_PATH}
+	echo "---------------------------------------------------------------------------"
+	php ${NEWZNAB_PATH}/optimise_db.php
+	php ${NEWZNAB_PATH}/update_tvschedule.php
+	php ${NEWZNAB_PATH}/update_theaters.php
+fi
+
+echo "==========================================================================="
+#echo "waiting ${NEWZNAB_SLEEP_TIME} seconds..."
+printf "$PRINTF_MASK" "waiting ${NEWZNAB_SLEEP_TIME} seconds..." "$YELLOW" "[${NEWZNAB_SLEEP_TIME} seconds]" "$RESET"
+echo "==========================================================================="
+sleep ${NEWZNAB_SLEEP_TIME}
+
 done
