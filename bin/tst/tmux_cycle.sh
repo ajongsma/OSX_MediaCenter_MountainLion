0e1683c12849e6b3893a172e07657e230841ce65
a.jongsma <a.jongsma@gmail.com>
2013-02-20 22:31:03 +0100

    Solarized

--- bin/tst/tmux_cycle.sh
+++ bin/tst/tmux_cycle.sh
@@ -3,19 +3,6 @@
 export TST_NEWZNAB_PATH="/Users/Newznab/Sites/newznab"
 export TST_SPOTWEB_PATH="/Users/Spotweb/Sites/spotweb"
 
-# This script assumes you have used the following 2 guides
-#  1) http://www.tiag.me/how-to-backfill-newznab-safely-without-bloating-your-database/
-#  2) https://sites.google.com/site/1204nnplus/optional-configurations
-# You can still use this script without having followed those guides but will have to edit to suit your application.
-# PLEASE NOTE: I used newznab_screen_backfill.sh instead of newznab_screen_local.sh You may need to change this!!!
-# Be sure to adjust paths to your install DIR
-# If you are not using Sphinx, Comment out the "Shpinx Startup" section
-# If you do not want to see Top, NIC stats, or Drive stats then comment out the "Monitoring Windows" section
-
-# http://www.jeffstory.org/wordpress/?p=132
-
-cd $TST_SPOTWEB_PATH
-
 tmux start-server
 tmux new-session -d -s NewzNab
 
@@ -25,8 +12,18 @@ tmux new-session -d -s NewzNab
 #tmux new-window -t NewzNab:4 -n runserver
 #tmux new-window -t NewzNab:5 -n utility
 
-tmux send-keys -t NewzNab:0 'cd $TST_SPOTWEB_PATH; clear' C-m
-tmux send-keys -t NewzNab:0 './spotweb_cycle.sh' C-m
+tmux select-pane -t 0
+tmux send-keys -t NewzNab:0 "cd $TST_SPOTWEB_PATH; ls" C-m
+tmux send-keys -t NewzNab:0 "./spotweb_cycle.sh" C-m
+
+#tmux splitw -v -p 25
+#tmux select-pane -t 1
+#tmux send-keys -tNewzNab:0 'top' C-m
+
+tmux select-window -t NewzNab:0
+tmux attach-session -d -t NewzNab
+
+
 #tmux send-keys -t NewzNab:1 'cd ~/Code/Python/django-haystack/tests/; export PYTHONPATH=`pwd`; clear' C-m
 #tmux send-keys -t NewzNab:2 'cd ~/Code/Python/all_django_projects/ds_recipe_testbed/my-solr; clear; ./run-solr.sh' C-m
 #tmux send-keys -t NewzNab:3 'cd ~/Code/Python/django-haystack/docs; clear' C-m
---
+++ scripts/install_solarized.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing Solarized"
+echo "#------------------------------------------------------------------------------"
+### https://github.com/xiaq/powerline-bash
+
+source ~/.bashrc
+
+git clone https://github.com/altercation/solarized.git ~/Github/Solarized
+
+open /Users/Andries/Github/Solarized/osx-terminal.app-colors-solarized/xterm-256color/Solarized\ Light\ xterm-256color.terminal
+open /Users/Andries/Github/Solarized/osx-terminal.app-colors-solarized/xterm-256color/Solarized\ Dark\ xterm-256color.terminal
+
+open /Users/Andries/Github/Solarized/iterm2-colors-solarized/Solarized\ Light.itermcolors
+open /Users/Andries/Github/Solarized/iterm2-colors-solarized/Solarized\ Dark.itermcolors
+
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing Solarized - Complete"
+echo "#------------------------------------------------------------------------------"
