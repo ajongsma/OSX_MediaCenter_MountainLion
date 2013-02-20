99e730839770d05535a3e92e454b56fbba8bd8cc
a.jongsma <a.jongsma@gmail.com>
2013-02-20 07:22:21 +0100

    Minor

--- bin/newznab_cycle.sh
+++ bin/newznab_cycle.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
 # call this script from within screen to get binaries, processes releases and 
 # every half day get tv/theatre info and optimise the database
 
-set -e
+#set -e
+
+RUN_PARSING="true"
+REMOVESPECIAL="true"
 
 export NEWZNAB_PATH="/Users/Newznab/Sites/newznab/misc/update_scripts"
 export NEWZNAB_TESTING_PATH="/Users/Newznab/Sites/newznab/misc/testing"
@@ -31,25 +35,44 @@ CURRTIME=`date +%s`
 cd ${NEWZNAB_PATH}
 printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_binaries]" "$RESET"
 php ${NEWZNAB_PATH}/update_binaries.php
-printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_binaries]" "$RESET"
+if [ "$?"-ne 0]; then
+    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_binaries]" "$RESET"
+else
+    printf "$PRINTF_MASK" "Processing failed!" "$ERROR" "[update_binaries]" "$RESET"
+fi
 
 echo "---------------------------------------------------------------------------"
 printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_releases]" "$RESET"
 php ${NEWZNAB_PATH}/update_releases.php
-printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
+if [ "$?"-ne 0]; then
+    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
+else
+    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[update_releases]" "$RESET"
+fi
 
 cd ${NEWZNAB_TESTING_PATH}
-echo "---------------------------------------------------------------------------"
-printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_parsing]" "$RESET"
-php ${NEWZNAB_TESTING_PATH}/update_parsing.php
-printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
 
-echo "---------------------------------------------------------------------------"
-printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[removespecial" "$RESET"
-php ${NEWZNAB_TESTING_PATH}/removespecial.php
-printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[removespecial]" "$RESET"
-#php /Users/Newznab/Sites/newznab/misc/testing/update_parsing.php
+if [[ $RUN_PARSING == "true" ]]; then
+	echo "---------------------------------------------------------------------------"
+	printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_parsing]" "$RESET"
+	php ${NEWZNAB_TESTING_PATH}/update_parsing.php
+	if [ "$?"-ne 0]; then
+	    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_releases]" "$RESET"
+	else
+	    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[update_releases]" "$RESET"
+	fi
+fi
 
+if [[ $REMOVESPECIAL == "true" ]]; then
+	echo "---------------------------------------------------------------------------"
+	printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[removespecial" "$RESET"
+	php ${NEWZNAB_TESTING_PATH}/removespecial.php
+	if [ "$?"-ne 0]; then
+	    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[removespecial]" "$RESET"
+	else
+	    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[removespecial]" "$RESET"
+	fi
+fi
 
 DIFF=$(($CURRTIME-$LASTOPTIMIZE))
 if [ "$DIFF" -gt 43200 ] || [ "$DIFF" -lt 1 ]
@@ -58,14 +81,36 @@ then
 
 	cd ${NEWZNAB_PATH}
 	echo "---------------------------------------------------------------------------"
-	php ${NEWZNAB_PATH}/optimise_db.php
+	printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[optimise_db" "$RESET"
+	php php ${NEWZNAB_PATH}/optimise_db.php
+	if [ "$?"-ne 0]; then
+	    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[optimise_db]" "$RESET"
+	else
+	    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[optimise_db]" "$RESET"
+	fi
+
+	echo "---------------------------------------------------------------------------"
+	printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_tvschedule]" "$RESET"
 	php ${NEWZNAB_PATH}/update_tvschedule.php
+	if [ "$?"-ne 0]; then
+	    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_tvschedule]" "$RESET"
+	else
+	    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[update_tvschedule]" "$RESET"
+	fi
+
+	echo "---------------------------------------------------------------------------"
+	printf "$PRINTF_MASK" "Starting process" "$YELLOW" "[update_theaters]" "$RESET"
 	php ${NEWZNAB_PATH}/update_theaters.php
+	if [ "$?"-ne 0]; then
+	    printf "$PRINTF_MASK" "Processing complete" "$GREEN" "[update_theaters]" "$RESET"
+	else
+	    printf "$PRINTF_MASK" "Processing failed!" "$RED" "[update_theaters]" "$RESET"
+	fi
 fi
 
 echo "==========================================================================="
 #echo "waiting ${NEWZNAB_SLEEP_TIME} seconds..."
-printf "$PRINTF_MASK" "waiting ${NEWZNAB_SLEEP_TIME} seconds..." "$YELLOW" "[${NEWZNAB_SLEEP_TIME} seconds]" "$RESET"
+printf "$PRINTF_MASK" "$(date) - NewzNAB update waiting for ${NEWZNAB_SLEEP_TIME} seconds..." "$YELLOW" "[${NEWZNAB_SLEEP_TIME} seconds]" "$RESET"
 echo "==========================================================================="
 sleep ${NEWZNAB_SLEEP_TIME}
 
