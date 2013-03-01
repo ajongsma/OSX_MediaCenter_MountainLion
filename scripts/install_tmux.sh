1af40f72356ea2afa04af449ca5e0ad7cfa4a6dc
ajongsma <a.jongsma@gmail.com>
2013-03-01 09:29:07 +0100

    Update install_tmux.sh

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -6,13 +6,14 @@ echo "#-------------------------------------------------------------------------
 
 brew install tmux
 
+[ -d ~/.tmux/conf ] || mkdir -p ~/.tmux/conf
 [ -d ~/.tmux/tmthemes ] || mkdir -p ~/.tmux/tmthemes
 [ -d ~/.tmux/segment ] || mkdir -p ~/.tmux/segment
 
-[ -f ~/.tmux/tmux_bash.conf ] || cp -v conf/tmux/tmux_bash.conf ~/.tmux/
-[ -f ~/.tmux/tmux.bindings.conf ] || cp -v conf/tmux/tmux.bindings.conf ~/.tmux/
-[ -f ~/.tmux/tmux.mouse.conf ] || cp -v conf/tmux/tmux.mouse.conf ~/.tmux/
-#[ -f ~/.tmux/tmux.powerline.conf ] || cp -v tmux.powerline.conf ~/.tmux/
+[ -f ~/.tmux/conf/tmux_bash.conf ] || cp -v conf/tmux/tmux_bash.conf ~/.tmux/conf/
+[ -f ~/.tmux/conf/tmux.bindings.conf ] || cp -v conf/tmux/tmux.bindings.conf ~/.tmux/conf/
+[ -f ~/.tmux/conf/tmux.mouse.conf ] || cp -v conf/tmux/tmux.mouse.conf ~/.tmux/conf/
+#[ -f ~/.tmux/conf/tmux.powerline.conf ] || cp -v tmux.powerline.conf ~/.tmux/conf/
 
 
 echo "#------------------------------------------------------------------------------"
