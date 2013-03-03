f744a454ab4a4381a1488b04827dde2330a9432b
a.jongsma <a.jongsma@gmail.com>
2013-03-03 22:30:15 +0100

    Minor

--- bin/tmux_spotweb_sync.sh
+++ bin/tmux_spotweb_sync.sh
@@ -68,6 +68,7 @@ else
 	printf "\033]0; $TMUX_SESSION\007\003\n"
 	#$TMUX_CMD -f $TMUX_CONF new-session -d -s $TMUX_SESSION -n $TMUX_SESSION 'cd bin && echo "Monitor Started" && echo "Spinning up..." && $NICE -n 19 $PHP monitor.php'
 
+	tmux start-server
 	tmux -f $TMUX_CONF new-session -d -s $TMUX_SESSION -n $TMUX_SESSION
 	#tmux attach-session -d -t Spotweb
 
