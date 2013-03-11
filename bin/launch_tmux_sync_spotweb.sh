709d9a4bd3891d17ba3ce56c0d54cc41b7e0f5e4
ajongsma <a.jongsma@gmail.com>
2013-03-11 13:00:59 +0100

    Update launch_tmux_sync_spotweb.sh

--- bin/launch_tmux_sync_spotweb.sh
+++ bin/launch_tmux_sync_spotweb.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+ 
+# NOTE: this is an OSX launchd wrapper shell script for Tomcat (to be placed in $CATALINA_HOME/bin)
 
-#pref=test_pref
-#pid=`pgrep spotweb`
+SPOTWEB_HOME=$HOME/bin
+ 
+function shutdown() {
+    date
+    echo "Shutting down Spotweb"
+    $SPOTWEB_HOME/bin/spotweb.sh stop
+}
+ 
+date
+echo "Starting Spotweb"
+export SPOTWEB_PID=/temp/$$
+ 
+. $SPOTWEB_HOME/bin/spotweb.sh start
+ 
+# Allow any signal which would kill a process to stop Tomcat
+trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
+ 
+echo "Waiting for `cat $SPOTWEB_PID`"
+wait `cat $SPOTWEB_PID`
 
-PID_SPOTWEB=`pgrep -f spotweb | head -n1`;
-
-[ -z $PID_SPOTWEB ] && $HOME/.bin/tmux_sync_spotweb.sh
+## OLD ---------------------
+##!/bin/bash
+#
+##pref=test_pref
+##pid=`pgrep spotweb`
+#
+#PID_SPOTWEB=`pgrep -f spotweb | head -n1`;
+#
+#[ -z $PID_SPOTWEB ] && $HOME/.bin/tmux_sync_spotweb.sh
