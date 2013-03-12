cd631385a6c9012bf91a7f4ccc82db06f8fbf272
ajongsma <a.jongsma@gmail.com>
2013-03-12 10:59:12 +0100

    Update install_headphones.sh

--- scripts/install_headphones.sh
+++ scripts/install_headphones.sh
@@ -68,39 +68,32 @@ open http://localhost/newznab/admin/role-edit.php?action=add
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
 read -n 1 -s
 
-if [ -f $DIR/conf/launchctl/com.headphones.headphones.plist ] ; then
-    echo "Copying Lauch Agent file:"
-    cp $DIR/launchctl/com.headphones.headphones.plist ~/Library/LaunchAgents/
+INST_FILE_LAUNCHAGENT="com.headphones.headphones.plist"
+if [ -f $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT ] ; then
+    echo "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT"
+    cp $DIR/launchctl/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
+    if [ "$?" != "0" ]; then
+        echo -e "${RED}  ============================================== ${RESET}"
+        echo -e "${RED} | ERROR ${RESET}"
+        echo -e "${RED} | Copy failed: ${RESET}"
+        echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+        echo -e "${RED} | --- press any key to continue --- ${RESET}"
+        echo -e "${RED}  ============================================== ${RESET}"
+        read -n 1 -s
+        exit 1
+    fi
 else
-    echo "Creating Lauch Agent file:"
-    cat >> /tmp/com.headphones.headphones.plist <<'EOF'
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>KeepAlive</key>
-	<dict>
-		<key>SuccessfulExit</key>
-		<true/>
-	</dict>
-	<key>Label</key>
-	<string>com.headphones.headphones</string>
-	<key>ProgramArguments</key>
-	<array>
-		<string>/usr/bin/python</string>
-		<string>/Users/Headphones/Sites/headphones/Headphones.py</string>
-	</array>
-	<key>RunAtLoad</key>
-	<true/>
-	<key>WorkingDirectory</key>
-	<string>/Users/Headphones/Sites/headphones</string>
-</dict>
-</plist>
-EOF
-    sudo mv /tmp/com.headphones.headphones.plist ~/Library/LaunchAgents/
+    echo -e "${RED}  ============================================== ${RESET}"
+    echo -e "${RED} | ERROR ${RESET}"
+    echo -e "${RED} | LaunchAgent file not found: ${RESET}"
+    echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+    echo -e "${RED} | --- press any key to continue --- ${RESET}"
+    echo -e "${RED}  ============================================== ${RESET}"
+    read -n 1 -s
+    sudo mv /tmp/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
 fi
+launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
 
-launchctl load ~/Library/LaunchAgents/com.headphones.headphones.plist
 
 echo "#------------------------------------------------------------------------------"
 echo "# Installing Headphones - Complete"
