aea586a2bfe720041da0836566334ceaf25a9ed8
ajongsma <a.jongsma@gmail.com>
2013-03-13 13:03:59 +0100

    Update launchd_wrapper.sh

--- bin/tst/launchd_wrapper.sh
+++ bin/tst/launchd_wrapper.sh
@@ -6,17 +6,21 @@
 
 WRAPPED_NAME="Spotweb"
 WRAPPED_PRG="launch_tmux_sync_spotweb.sh"
-WRAPPED_ROOT=/usr/local/bin
+WRAPPED_BASE=/usr/local/bin
 
-if [ ! -d "$WRAPPED_ROOT" ]; then
-  echo "Error! $WRAPPED_ROOT does not exist"
+if [ -z "$WRAPPED_HOME" ] ; then
+  WRAPPED_HOME="$WRAPPED_BASE"
+fi
+
+if [ ! -d "$WRAPPED_HOME" ]; then
+  echo "Error! $WRAPPED_HOME does not exist"
   exit 1
 fi
 
 function shutdown() {
     date
     echo "Shutting down $WRAPPED_NAME"
-    $WRAPPED_ROOT/$WRAPPED_PRG stop
+    $WRAPPED_HOME/$WRAPPED_PRG stop
     echo "done."
 
     # Cleaning up the temporary file
@@ -27,7 +31,7 @@ date
 echo "Starting $WRAPPED_NAME Updater"
 WRAPPED_PID=/tmp/$$
  
-. $WRAPPED_ROOT/$WRAPPED_PRG start
+. $WRAPPED_HOME/$WRAPPED_PRG start
  
 # Allow any signal which would kill process
 trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
