fa3f59e30f28e72e015cb234d58e9ed846d7e2d0
a.jongsma <a.jongsma@gmail.com>
2013-03-15 18:34:08 +0100

    LaunchAgent NewzNAB plus scripts

--- scripts/install_newznab.sh
+++ scripts/install_newznab.sh
@@ -160,23 +160,50 @@ fi
 
 echo "-----------------------------------------------------------"
 echo "| Update file update_parsing.php:"
-echo "| \$echo = true;                 : \$echo = false;
+echo "| \$echo = true;                 : \$echo = false;"
 echo "-----------------------------------------------------------"
 subl $INST_NEWZNAB_PATH/misc/testing/update_parsing.php
 
 cd $INST_NEWZNAB_PATH/misc/update_scripts/nix_scripts/
 sh ./newznab_local.sh
-#bash ./newznab_local.sh
 
-#cd /Users/Newznab/Sites/newznab/misc/update_scripts
-##php update_binaries.php && php update_releases.php
-##php update_binaries.php
-##php update_releases.php
 
-# screen bash
-#screen
-#./newznab_local.sh
-#echo "ctrl-ad to detach screen"
+# -----------------------------------------------------------"
+#| Installing LaunchAgent plus scripts
+# -----------------------------------------------------------"
+[ -d /usr/local/share/newznab ] || mkdir -p /usr/local/share/newznab
+cp /Users/Andries/Github/OSX_NewBox/bin/share/tmux_sync_newznab.sh /usr/local/share/newznab
+cp /Users/Andries/Github/OSX_NewBox/bin/share/newznab_cycle.sh /usr/local/share/newznab
+ln -s /usr/local/share/newznab/tmux_sync_newznab.sh /usr/local/bin/
+
+[ -d /var/log/newznab ] || sudo mkdir -p /var/log/newznab && sudo chown `whoami` /var/log/newznab
+
+INST_FILE_LAUNCHAGENT="com.tmux.newznab.plist"
+if [ -f $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT ] ; then
+    echo "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT"
+    cp $DIR/launchctl/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
+    if [ "$?" != "0" ]; then
+        echo -e "${RED}  ============================================== ${RESET}"
+        echo -e "${RED} | ERROR ${RESET}"
+        echo -e "${RED} | Copy failed: ${RESET}"
+        echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+        echo -e "${RED} | --- press any key to continue --- ${RESET}"
+        echo -e "${RED}  ============================================== ${RESET}"
+        read -n 1 -s
+        exit 1
+    fi
+    launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
+else
+    echo -e "${RED}  ============================================== ${RESET}"
+    echo -e "${RED} | ERROR ${RESET}"
+    echo -e "${RED} | LaunchAgent file not found: ${RESET}"
+    echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+    echo -e "${RED} | --- press any key to continue --- ${RESET}"
+    echo -e "${RED}  ============================================== ${RESET}"
+    read -n 1 -s
+    exit 1
+fi
+
 
 #------------------------------------------------------------------------------
 # Install NewzNAB - Complete
