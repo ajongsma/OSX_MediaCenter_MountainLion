f23f2f10431225d29a5474a078721164b6479ff5
ajongsma <a.jongsma@gmail.com>
2013-04-02 11:33:27 +0300

    Update install_brew.sh

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -7,10 +7,13 @@ echo "#-------------------------------------------------------------------------
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 if [ ! -e /usr/local/bin/brew ] ; then
     printf 'HomeBrew failed installing\n' "$RED" $col '[FAIL]' "$RESET"
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
     exit 1
 else
     printf 'HomeBrew found\n' "$GREEN" $col '[OK]' "$RESET"
 fi
+
 # Make sure we’re using the latest Homebrew
 brew update
 
