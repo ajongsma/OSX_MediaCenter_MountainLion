5a51c7375928d00e570217f728cd48bf28c519ba
ajongsma <a.jongsma@gmail.com>
2013-04-05 11:50:38 +0300

    Update install_sabnzbd_webapp.sh

--- scripts/install_sabnzbd_webapp.sh
+++ scripts/install_sabnzbd_webapp.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 echo "#------------------------------------------------------------------------------"
-echo "# Installing SABnzbd - Apache webapp"
+echo "# Installing Apache webapp for SABnzbd"
 echo "#------------------------------------------------------------------------------"
 
 source ../config.sh
 
 if [ -d /Library/Server/Web/Config/apache2/webapps ] ; then
-	echo "Apache WebApp directory found. Copying SABnzbd webapp config file"
+    printf 'Apache WebApp directory found. Copying SABnzbd webapp config file...\n' "YELLOW" $col '[WAIT]' "$RESET"
     cp conf/webapps/org.sabnzbd.plist /Library/Server/Web/Config/apache2/webapps/
     if [ ! -f /Library/Server/Web/Config/apache2/webapps/org.sabnzbd.plist ] ; then
-
-	    echo "SABnzbd webapp config file found. Enabling..."
-	    sudo webappctl start /Library/Server/Web/Config/apache2/webapps/org.sabnzbd.plist
-	else
-		echo "SABnzbd webapp config file not found. Failed!"
-	fi
+        printf 'SABnzbd webapp config file found. Enabling...\n' "YELLOW" $col '[WAIT]' "$RESET"
+        sudo webappctl start /Library/Server/Web/Config/apache2/webapps/org.sabnzbd.plist
+    else
+    	printf 'SABnzbd webapp config file not found. Failed!\n' "RED" $col '[ERR]' "$RESET"
+        echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    fi
 fi
 
 echo "#------------------------------------------------------------------------------"
-echo "# Install SABnzbd - Apache webapp - Complete"
+echo "# Install Apache webapp for SABnzbd - Complete"
 echo "#------------------------------------------------------------------------------"
