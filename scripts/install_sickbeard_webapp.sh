a9f5fbfda19b88d22322096ac28ec1c13cc64ff2
a.jongsma <a.jongsma@gmail.com>
2013-04-05 17:56:05 +0200

    Webapp Sickbeard

--- scripts/install_sickbeard_webapp.sh
+++ scripts/install_sickbeard_webapp.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 echo "#------------------------------------------------------------------------------"
-echo "# Installing Apache webapp for SABnzbd"
+echo "# Installing Apache webapp for Sick-Beard"
 echo "#------------------------------------------------------------------------------"
 
 source ../config.sh
 
 if [ -d /Library/Server/Web/Config/apache2/webapps ] ; then
-    printf 'Apache WebApp directory found. Copying SABnzbd webapp config file...\n' "YELLOW" $col '[WAIT]' "$RESET"
+    printf 'Apache WebApp directory found. Copying Sick-Beard webapp config file...\n' "YELLOW" $col '[WAIT]' "$RESET"
     sudo cp -iv conf/webapps/org.sabnzbd.plist /Library/Server/Web/Config/apache2/webapps/
-    if [ ! -f /Library/Server/Web/Config/apache2/webapps/org.sabnzbd.plist ] ; then
-        printf 'SABnzbd webapp config file found. Enabling...\n' "YELLOW" $col '[WAIT]' "$RESET"
-        cd /Library/Server/Web/Config/apache2/webapps/
-        sudo webappctl start org.sabnzbd
+    if [ ! -f /Library/Server/Web/Config/apache2/webapps/org.sickbeard.plist ] ; then
+        printf 'Sick-Beard webapp config file found. Enabling...\n' "YELLOW" $col '[WAIT]' "$RESET"
+        cd Library/Server/Web/Config/apache2/webapps/
+        sudo webappctl start org.sickbeard
 
-        open http://localhost/sabnzbd/
+        open http://localhost/sickbeard
     else
-    	printf 'SABnzbd webapp config file not found. Failed!\n' "RED" $col '[ERR]' "$RESET"
+    	printf 'Sick-Beard webapp config file not found. Failed!\n' "RED" $col '[ERR]' "$RESET"
         echo -e "${BLUE} --- press any key to continue --- ${RESET}"
         read -n 1 -s
     fi
 fi
 
 echo "#------------------------------------------------------------------------------"
-echo "# Install Apache webapp for SABnzbd - Complete"
+echo "# Install Apache webapp for Sick-Beard - Complete"
 echo "#------------------------------------------------------------------------------"
