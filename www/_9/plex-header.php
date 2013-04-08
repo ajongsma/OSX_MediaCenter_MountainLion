4a95c531ebf6c878246d1e5ed0818acec3e161c4
a.jongsma <a.jongsma@gmail.com>
2013-04-08 17:50:31 +0200

    Minor

--- www/_9/plex-header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "<br><br>";
-echo "<a href='plex-movies.php'>PlexMovies</a> | <a href='plex-shows.php'>PlexShows</a> | <a href='plex-radded.php'>Plex Recently Added</a> ";
-echo "<br><br>";
-
-?>
