31ce1e33a16ed33cec2a8f14d9b34bd4f5c36167
ajongsma <a.jongsma@gmail.com>
2013-04-09 14:45:20 +0300

    Update index.php

--- www/_1/tmp/sickbeard/index.php
+++ www/_1/tmp/sickbeard/index.php
@@ -22,6 +22,7 @@
 </head>
 <body>
 	<div id='site_content'>
+    <center>
 <?php
 
 $apiURL = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=shows&sort=name&paused=0";
@@ -39,158 +40,135 @@ foreach ($sbJSON_Shows['data'] as $key => $values) {
   $apiURL_sbShow = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show&tvdbid=".$showid;
   $apiURL_TraktTV = "http://api.trakt.tv/show/episode/summary.json/".$trakt_api."/".$showid."/1/1";
 
-	// Fetch TraktTV api
-	if ($trakt_enabled == "true") {
-		$sbJSON = json_decode(file_get_contents($apiURL_sbSeasonList));
-		$tvdata = json_decode(file_get_contents($apiURL_sbShow));
-		
-		$trakt = json_decode(file_get_contents($apiURL_TraktTV));
-	} else {
-		$sbJSON = json_decode(file_get_contents($apiURL_sbSeasonList));
-		$tvdata = json_decode(file_get_contents($apiURL_sbShow));
-	}
+  // Fetch TraktTV api
+  if ($trakt_enabled == "true") {
+  	$sbJSON = json_decode(file_get_contents($apiURL_sbSeasonList));
+  	$tvdata = json_decode(file_get_contents($apiURL_sbShow));
+  	
+  	$trakt = json_decode(file_get_contents($apiURL_TraktTV));
+  } else {
+  	$sbJSON = json_decode(file_get_contents($apiURL_sbSeasonList));
+  	$tvdata = json_decode(file_get_contents($apiURL_sbShow));
+  }
+  
+  // Grab Show Title
+  $title = $tvdata->{data}->{show_name};
 
-	// Grab Show Title
-	$title = $tvdata->{data}->{show_name};
-
-	echo "<center>";
-	echo "<h1>(1) ".$title."</h1>";
+  echo "<h1>(1) ".$title."</h1>";
 
   if ($display_img_banners == "true") {	
-    // trakt.tv banner intragration
+    // Show Trakt.TV banner
   	if ($trakt_enabled == "true") {
   		if ($trakt->{status} == "failure") {
-  			// Display SickBeard Banner as trakt returned an error
+  			// Show SickBeard Banner if trakt returned an error
   			printf("<img src=http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show.getbanner&tvdbid=".$showid."><br><br>");
   		} else {
-  			// Display trakt.tv Bannger
+  			// Show trakt.tv Bannger
   			printf("<img src=".$trakt->{show}->{images}->{banner}."><br><br>");
   		}
   	} else {
-  		// Display SickBeard Banner
+  		// Show SickBeard Banner
   		printf("<img src=http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show.getbanner&tvdbid=".$showid."><br><br>");
   	}
   }
   
-	if ($trakt_enabled == "1") {
+	if ($trakt_enabled == "true") {
 		echo "<b>Year:</b> ".$trakt->{show}->{year}." | <b>Country:</b> ".$trakt->{show}->{country}."<br>";
 		echo "<b>Network:</b> ".$trakt->{show}->{network}." | <b>Run Time:</b> ".$trakt->{show}->{runtime}." Mins<br>";
 		echo "<b>Runs:</b> ".$trakt->{show}->{air_day}.", ".$trakt->{show}->{air_time}."<br>";
 		echo "<b>Overview:</b> ".$trakt->{show}->{overview}."<br><br>";
 	} else {
-	// Fix Next Ep
-	if ($tvdata->{data}->{next_ep_airdate} == "")
-		{
+  	// Show Sickbeard Next Episode
+  	if ($tvdata->{data}->{next_ep_airdate} == "") {
 			$next_ep = "N/A";
-		}
-	else
-		{
+		} else {
 			$next_ep = $tvdata->{data}->{next_ep_airdate};
 		}
-	// Show Details
-	echo "Network: ".$tvdata->{data}->{network}.", Airs: ".$tvdata->{data}->{airs}.", Next Ep: ".$next_ep.", Show Status: ".$tvdata->{data}->{status}."<br><br>";
+  	// Show Details Next Episode
+  	echo "Network: ".$tvdata->{data}->{network}.", Airs: ".$tvdata->{data}->{airs}.", Next Ep: ".$next_ep.", Show Status: ".$tvdata->{data}->{status}."<br><br>";
 	}
 
 	// Run through each feed item
 	foreach($sbJSON->{data} as $show) {
 
-
-	    // Show Details
-	    if ($show == '0')
-	    {
-	    	echo "<a href='episode.php?showid=".$showid."&seasonid=".$show."'>Specials </a><br />";
-	    }
-	    else
-	    {
-	    	echo "<a href='episode.php?showid=".$showid."&seasonid=".$show."'>Season ".$show." </a><br />";
-	    }
+    // Show Details
+    if ($show == '0') {
+    	echo "<a href='episode.php?showid=".$showid."&seasonid=".$show."'>Specials </a><br />";
+    } else {
+    	echo "<a href='episode.php?showid=".$showid."&seasonid=".$show."'>Season ".$show." </a><br />";
+    }
 
 #============ (2 START) ------------------------------------------------------
-	    $seasonid = $show;
-		// Check if username is available, set URL
-		    $feed3_1 = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show.seasons&tvdbid=".$showid."&season=".$seasonid;
-		    $feed3_2 = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show&tvdbid=".$showid;
-		    $feed3_3 = "http://api.trakt.tv/show/episode/summary.json/".$trakt_api."/".$showid."/1/1";
-		    echo $feed3_1."<br>";
-		    echo $feed3_2."<br>";
-		    echo $feed3_3."<br>";
+    $seasonid = $show;
+    
+    // Check if username is available, set URL
+    $feed3_1 = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show.seasons&tvdbid=".$showid."&season=".$seasonid;
+    $feed3_2 = "http://".$sickbeard_host.":".$sickbeard_port."/api/".$sickbeard_api."/?cmd=show&tvdbid=".$showid;
+    $feed3_3 = "http://api.trakt.tv/show/episode/summary.json/".$trakt_api."/".$showid."/1/1";
+    echo $feed3_1."<br>";
+    echo $feed3_2."<br>";
+    echo $feed3_3."<br>";
 
-		    
 		// fetch trakt api
-		if ($trakt_enabled == "1")
-		{
+		if ($trakt_enabled == "true") {
 			$sbJSON = json_decode(file_get_contents($feed3_1));
 			$tvdata = json_decode(file_get_contents($feed3_2));
 			$trakt = json_decode(file_get_contents($feed3_3));
-		}
-		else
-		{
+		} else {
 			$sbJSON = json_decode(file_get_contents($feed3_1));
 			$tvdata = json_decode(file_get_contents($feed3_2));
 		}
 
+  	// Grab Show Title
+		$title = $tvdata->{data}->{show_name};
+    
 echo "<table>";
 echo "  <tr>";
 echo "    <td>";
-		// Grab Show Title
-		$title = $tvdata->{data}->{show_name};
-
-		//Display Browser Title
-		echo "<title>".$title." | Season ".$seasonid." | ".$site_name."</title>";
-
+echo "      <h1>".$title." Season ".$seasonid."</h1>";
 echo "    </td>";
 echo "  </tr>";
 echo "  <tr>";
-echo "    <td>";		
-		echo "<center>";
-
-		// What are you!?
-		echo "<h1>".$title." Season ".$seasonid."</h1>";
-
-		// trakt.tv banner intragration
-		if ($trakt_enabled == "1")
-		{
-			printf("<img src=".$trakt->{show}->{images}->{banner}."><br><br>");
-		}
-		else
-		{
-			// Display Show Bannger
-			printf("<img src=http://".$ip."/api/".$api."/?cmd=show.getbanner&tvdbid=".$showid."><br><br>");
-		}
 
 		// Define episode counter
 		$counter = "1";
 
 		// Run through each feed item
 		foreach($sbJSON->{data} as $show) {
+      // Show Details
+echo "    <td>";
+echo "      <a href='epdata.php?showid=".$showid."&seasonid=".$seasonid."&ep=".$counter."'><b>Episode:</b> " . $counter . "</a><br />";
+echo "    </td>";
+echo "    <td>";
+      echo "<b>Name:</b> " . $show->{name} . "<br />";
+echo "    </td>";
+echo "    <td>";
+      echo "<b>Aired:</b> " . $show->{airdate} . "<br />";
+echo "    </td>";
+echo "    <td>";
+      if ($show->{status} == "Archived")
+      {
+      	echo "<font color='#41A317'><b>Status:</b> Collected </font><br /><br />";
+      } 
+      elseif ($show->{status} == "Snatched")
+      {
+      	echo "<font color='#41A317'><b>Status:</b> Downloading... </font><br /><br />";
+      }
+      elseif ($show->{status} == "Downloaded")
+      {
+      	echo "<font color='#41A317'><b>Status:</b> Collected </font><br /><br />";
+      }
+      elseif ($show->{status} == "Wanted")
+      {
+      	echo "<font color='#306EFF'><b>Status:</b> Wanted </font><br /><br />";
+      }
+      else
+      {
+      	echo "<font color='#F62817'><b>Status:</b> Not Collected </font><br /><br />";
+      }
+      $counter = $counter + "1";
+    }
 
-		        // Show Details
-		        echo "<a href='epdata.php?showid=".$showid."&seasonid=".$seasonid."&ep=".$counter."'><b>Episode:</b> " . $counter . "</a><br />";
-		        echo "<b>Name:</b> " . $show->{name} . "<br />";
-		        echo "<b>Aired:</b> " . $show->{airdate} . "<br />";
-		        if ($show->{status} == "Archived")
-		        {
-		        	echo "<font color='#41A317'><b>Status:</b> Collected </font><br /><br />";
-		        }
-		        elseif ($show->{status} == "Snatched")
-		        {
-		        	echo "<font color='#41A317'><b>Status:</b> Downloading... </font><br /><br />";
-		        }
-		        elseif ($show->{status} == "Downloaded")
-		        {
-		        	echo "<font color='#41A317'><b>Status:</b> Collected </font><br /><br />";
-		        }
-		        elseif ($show->{status} == "Wanted")
-		        {
-		        	echo "<font color='#306EFF'><b>Status:</b> Wanted </font><br /><br />";
-		        }
-		        else
-		        {
-		        	echo "<font color='#F62817'><b>Status:</b> Not Collected </font><br /><br />";
-		        }
-
-		        $counter = $counter + "1";
-		    }
 echo "    </td>";
 echo "  </tr>";
 echo "</table>";
