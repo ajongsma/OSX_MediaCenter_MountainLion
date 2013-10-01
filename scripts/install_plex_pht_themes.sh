47b2ee4e7e5b70ef18921a345a51011e32c6ac77
ajongsma <a.jongsma@gmail.com>
2013-10-01 22:00:19 +0200

    Update install_plex_pht_themes.sh

--- scripts/install_plex_pht_themes.sh
+++ scripts/install_plex_pht_themes.sh
@@ -6,3 +6,5 @@ echo "#-------------------------------------------------------------------------
 
 cd Library/Application\ Support/Plex\ Home\ Theater/addons/
 git clone https://github.com/maverick214/skin.PlexAeonPHT
+
+git clone https://github.com/maverick214/skin.MetropolisPHT
