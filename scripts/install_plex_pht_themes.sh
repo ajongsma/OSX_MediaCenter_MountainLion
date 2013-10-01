059f95cc0b9045b264548f5569cdf38f05374e20
ajongsma <a.jongsma@gmail.com>
2013-10-01 21:56:31 +0200

    Update install_plex_pht_themes.sh

--- scripts/install_plex_pht_themes.sh
+++ scripts/install_plex_pht_themes.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing Themes for Plex Home Theater"
+echo "#------------------------------------------------------------------------------"
+
 cd Library/Application\ Support/Plex\ Home\ Theater/addons/
 git clone https://github.com/maverick214/skin.PlexAeonPHT
