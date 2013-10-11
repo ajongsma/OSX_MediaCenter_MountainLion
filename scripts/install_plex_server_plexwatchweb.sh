f115138823e0faafe08f7733cd275c9d5ee80c1e
ajongsma <a.jongsma@gmail.com>
2013-10-11 14:24:07 +0200

    Create install_plex_server_plexwatchweb.sh

---
+++ scripts/install_plex_server_plexwatchweb.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install plexWebWatch for Plex Media Server"
+echo "#------------------------------------------------------------------------------"
+
+source ../config.sh
+
+if [ -e /Applications/Plex\ Media\ Server.app ] ; then
+    printf 'Plex Server found\n' "$GREEN" $col '[OK]' "$RESET"
+
+    [ -d /Users/PlexWatch/Sites/plexwatch ] || mkdir -p /Users/PlexWatch/Sites/plexwatch
+    sudo chown `whoami` /Users/PlexWatch/Sites/plexwatch
+    sudo ln -s /Users/PlexWatch/Sites/plexwatch /Library/Server/Web/Data/Sites/Default/plexwatch
+
+    cd /Users/PlexWatch/Sites/plexwatch
+    git clone https://github.com/ecleese/plexWatchWeb
+
+
+else
+    printf 'Plex Server not installed, something went wrong\n' "$RED" $col '[FAIL]' "$RESET"
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+    exit
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install plexWebWatch for Plex Media Server - Complete"
+echo "#------------------------------------------------------------------------------"
